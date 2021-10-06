5bbe974a0b508bdc106dca88b68c4e9505b599be
Your Name <ann19971996@gmail.com>
2021-10-06 19:21:10 +0800

    add four columns

--- sql/covid19_season.sql
+++ sql/covid19_season.sql
@@ -3,7 +3,9 @@ create table covidseason(
 	iso_code varchar(24) comment '國碼',
     location varchar(24) comment '國家',
     date_season varchar(24) comment '年季',
-    new_cases int default 0 comment '新增確診數',
-    new_cases_smoothed float(10,2) default 0.0 comment '七天移動平均新增確診數'
+    new_cases_smoothed float(10,2) default 0.0 comment '七天移動平均新增確診數',
+    reproduction_rate float(10,2) default 0.0 comment '傳染率',
+    positive_rate float(10,2) default 0.0 comment '陽性率',
+    people_fully_vaccinated_per_hundred float(10,2) default 0.0 comment '每百人接種疫苗人數'
 );
 drop table covidseason;
