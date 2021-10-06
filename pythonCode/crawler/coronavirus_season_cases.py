fe91bcc31f6b062b6bb52338eef26f5eb5037f54
Your Name <ann19971996@gmail.com>
2021-10-06 19:18:29 +0800

    add four columns

--- pythonCode/crawler/coronavirus_season_cases.py
+++ pythonCode/crawler/coronavirus_season_cases.py
@@ -17,15 +17,16 @@ data_season = {'20201':['2020-01-01', '2020-03-31'],
                '20213':['2021-07-01', '2021-09-30'],
                '20214':['2021-10-01', '2021-12-31']}
 for key, value in data_season.items():
-    sql1 = '''select aa.iso_code, aa.location, avg(aa.new_cases), avg(aa.new_cases_smoothed) from covid19details aa
+    sql1 = '''select aa.iso_code, aa.location, avg(aa.new_cases_smoothed), avg(aa.reproduction_rate), avg(aa.positive_rate), avg(aa.people_fully_vaccinated_per_hundred) from covid19details aa
         where date_t between '%s' AND '%s'
         group by 1; ''' \
         % (value[0], value[1])
     cur.execute(sql1)
     dataa = cur.fetchall()
     for data in dataa:
-        sql2 = '''INSERT INTO covidseason VALUES ('%s', '%s', '%s', '%d', '%f'); ''' \
-               % (data[0], data[1], key, data[2], data[3])
+        sql2 = '''INSERT INTO covidseason VALUES ('%s', '%s', '%s', '%f', '%f', '%f', '%f'); ''' \
+               % (data[0], data[1], key, data[2], data[3], data[4], data[5])
+        print(data[0], data[1], key, data[2], data[3], data[4], data[5])
         cur.execute(sql2)
         conn.commit()
 conn.close()
