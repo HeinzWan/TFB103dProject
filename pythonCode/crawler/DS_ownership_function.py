72b8647da0283f92e4811e470c329924cf728976
LeonLin <i88828702@gmail.com>
2021-10-31 11:34:07 +0800

    modify code add all companys

--- pythonCode/crawler/DS_ownership_function.py
+++ pythonCode/crawler/DS_ownership_function.py
@@ -1,9 +1,8 @@
-def DSownership(stockcode):
-    import requests
+def DSownership(stockcodes):
+    import requests, random, time
     from bs4 import BeautifulSoup
     import pandas as pd
     from sqlalchemy import create_engine
-    import time
     import configparser
 
     # config = configparser.ConfigParser()
@@ -17,46 +16,71 @@ def DSownership(stockcode):
     # # 建立連線引擎
     # engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}')
 
-    url = "https://goodinfo.tw/StockInfo/StockDirectorSharehold.asp?STOCK_ID={}".format(stockcode)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
-
-    res = requests.get(url, headers=headers)
-    # print(res.encoding)  => 編碼為ISO-8859-1
-    res.encoding = res.apparent_encoding  #解碼，中文才不會出現亂碼
-    soup = BeautifulSoup(res.text,'html.parser')
-
-    table = soup.select("table.b1.p4_0.r0_10.row_bg_2n.row_mouse_over tr")
-
-    datas = []
-    for i in table:
-        text = i.text.replace(",", "").replace('-', '-0').split(" ")[1:]
-        tmp = [stockcode]
-        tmp.extend(text)
-        if len(tmp) == 22:
-            tmp[1] += "/01"
-            datas.append(tmp)
-
     columns = ['stock_code', 'data_date', 'month_price', 'amount_of_change', 'rate_of_change',
                'total_board', 'director_amount', 'director_rate', 'director_dalta', 'director_pledge_amount',
                'director_pledge_rate', 'indirector_amount', 'indirector_rate', 'indirector_dalta',
-               'indirector_pledge_amount','indirector_pledge_rate', 'toldirector_amount', 'toldirector_rate',
-               'toldirector_dalta','toldirector_pledge_amount','toldirector_pledge_rate', 'foreign_rate']
+               'indirector_pledge_amount', 'indirector_pledge_rate', 'toldirector_amount', 'toldirector_rate',
+               'toldirector_dalta', 'toldirector_pledge_amount', 'toldirector_pledge_rate', 'foreign_rate']
 
-    df = pd.DataFrame(datas, columns=columns)
+    df = pd.DataFrame([], columns=columns)  # 輸出結果用的 DataFrame
+    for stockcode in stockcodes:
+        url = "https://goodinfo.tw/StockInfo/StockDirectorSharehold.asp?STOCK_ID={}".format(stockcode)
+        headers = {
+            # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'
+            }
 
-    engine = create_engine('mysql+pymysql://root:123456@localhost:3306/tfb103d_project')
+        res = requests.get(url, headers=headers)
+        print('status_code = ' + str(res.status_code))
+        # print(res.encoding)  => 編碼為ISO-8859-1
+        res.encoding = res.apparent_encoding  #解碼，中文才不會出現亂碼
+        soup = BeautifulSoup(res.text,'html.parser')
+        table = soup.select("table.b1.p4_0.r0_10.row_bg_2n.row_mouse_over tr")
 
+        datas = []
+        for i in table:
+            text = i.text.replace(",", "").replace('-', '-0').split(" ")[1:]
+            tmp = [stockcode]
+            tmp.extend(text)
+            # print(tmp)
+            if len(tmp) == 22:
+                tmp[1] += "/01"
+                datas.append(tmp)
+
+        df_tmp = pd.DataFrame(datas, columns=columns) # 建立 DataFrame
+        print(df_tmp)
+        if df_tmp.values.shape[0] == 0:
+            print(str(stockcode)+"Error!!!")
+            time.sleep(1800)
+
+        df = df.append(df_tmp, ignore_index=True)  # 將 DataFrame 加入輸出結果的 DataFrame 中
+
+        print("{},Successfully!".format(stockcode))
+
+        #隨機指定停留秒數，模擬人為使用的情況，避免被阻擋
+        sleep_time = random.randint(20,30)
+        print('sleep_times = ' + str(sleep_time))
+        time.sleep(sleep_time)
+
+    # 將輸出結果的 DataFrame 儲存至 SQL 資料表中
+    engine = create_engine('mysql+pymysql://root:ian1991@localhost:3306/tfb103d_project')
     df.to_sql('dsownership', engine, if_exists="append", index=False)
-
-    time.sleep(10) #太快會被網站阻擋，測試5秒間隔好像沒問題
 
     return df
 
-companys = ["2330","2303","2379","6488","5347","4966","2454","3529","3105","5483","3034"]
+# companys = ["2330","2303","2379","6488","5347","4966","2454","3529","3105","5483","3034"]
 
-for code in companys:
-    DSownership(code)
+companys = ["2330","2303","2379","6488","5347","4966","2454","3529","3105","5483","3034",
+            '3686','3711','4919','4952','4961','4967','8110','8131','8150','8261','8271'
+            ,'4968','5222','5269','5285','5471','6202','6239','6243','6257','6271','6415'
+            ,'6451','6515','6525','6531','6533','6552','6573','6756','8016','8028','8081'
+            ]
+
+print(len(companys))
+
+df = DSownership(companys)
+
+print(df)
 
 # if __name__ == "__main__":
 #     stockcode = '2330'
--- pythonCode/crawler/ownership_function.py
+++ pythonCode/crawler/ownership_function.py
@@ -1,5 +1,5 @@
-def ownership(stockcode):
-    import requests,pymysql
+def ownership(stockcodes):
+    import requests,pymysql, time, random
     from bs4 import BeautifulSoup
     import pandas as pd
     from sqlalchemy import create_engine
@@ -16,48 +16,78 @@ def ownership(stockcode):
     # # 建立連線引擎
     # engine = create_engine(f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}')
 
-    url = "https://norway.twsthr.info/StockHolders.aspx?stock={}".format(stockcode)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
 
-    res = requests.get(url, headers=headers)
-    soup = BeautifulSoup(res.text,'html.parser')
+    columns = ["stock_code", "data_date", "total_board",
+               "total_holder", "avg_board", "over400_amount",
+               "over400_rate", "over400_holder", "between400_600",
+               "between600_800", "between800_1000", "over1000_holder",
+               "over1000_rate", "closing_price"]
 
-    table = soup.select("div#D1 td")
+    df = pd.DataFrame([], columns=columns) #輸出結果用的 DataFrame
+    for stockcode in stockcodes:
+        url = "https://norway.twsthr.info/StockHolders.aspx?stock={}".format(stockcode)
 
-    #資料
-    datas = []
-    tmp = [stockcode]
-    for data in (table[17:-2]):
-        if len(tmp) < 14:
-            if data.text != "\xa0":
-                if data.text == '':
-                    t = data.text
-                    t += "0"
-                    tmp.append(t)
+        res = requests.get(url, headers=headers)
+        soup = BeautifulSoup(res.text,'html.parser')
+        table = soup.select("div#D1 td")
+
+        #資料
+        datas = []
+        tmp = [stockcode]
+        for data in (table[17:-2]):
+            if len(tmp) < 14: #共有 14 columns 的數值
+                if data.text != "\xa0": #非 \xa0的空白值才處理
+                    if data.text == '': #空值以 None 處理
+                        t = None
+                        # t += None
+                        tmp.append(t)
+                    else:
+                        # 字串處理:去掉\xa0的空白值並取代 ','
+                        tmp.append(data.text.split("\xa0")[0].replace(",", ""))
+                    #將每個 row 建立名為 tmp 的 list
+            else:
+                if float(tmp[4]) > 200.00: #部分原始資料有出現嚴重的錯誤，觀察後可以以"平均張數"欄位的數值判定後排除
+                    print(tmp) #呈現有錯誤的原始資料
+                    tmp = [stockcode] #將 tmp 回復
+                    pass
                 else:
-                    tmp.append(data.text.split("\xa0")[0].replace(",", ""))
+                    datas.append(tmp) #將 tmp 插入 datas (成為二維矩陣，用於建立 DataFrame)
+                    tmp = [stockcode] #將 tmp 回復
 
-        else:
-            datas.append(tmp)
-            tmp = [stockcode]
+        df_tmp = pd.DataFrame(datas, columns=columns) # 建立 DataFrame
+        print(df_tmp)
 
-    #合併為資料表
-    columns = ["stock_code","data_date","total_board",
-               "total_holder","avg_board","over400_amount",
-               "over400_rate","over400_holder","between400_600",
-               "between600_800","between800_1000","over1000_holder",
-               "over1000_rate","closing_price"]
+        df = df.append(df_tmp,ignore_index=True) # 將 DataFrame 加入輸出結果的 DataFrame 中
 
-    df = pd.DataFrame(datas, columns=columns)
+        print("{},Successfully!".format(stockcode))
+
+        #隨機指定停留秒數，模擬人為使用的情況，避免被阻擋
+        sleep_time = random.randint(3,8)
+        print('sleep_times = ' + str(sleep_time))
+        time.sleep(sleep_time)
+
+    #將輸出結果的 DataFrame 儲存至 SQL 資料表中
     engine = create_engine('mysql+pymysql://root:ian1991@localhost:3306/tfb103d_project')
     df.to_sql('ownership', engine, if_exists="append", index=False)
-    return "{},Successfully!".format(stockcode)
+
+    return df
 
 # 股權分散圖
-companys = ["2330","2303","2379","6488","5347","4966","2454","3529","3105","5483","3034"]
-for code in companys:
-    ownership(code)
+# companys = ["2330","2303","2379","6488","5347","4966","2454","3529","3105","5483","3034"]
+
+companys = ["2330","2303","2379","6488","5347","4966","2454","3529","3105","5483","3034",
+            '3686','3711','4919','4952','4961','4967','8110','8131','8150','8261','8271'
+            ,'4968','5222','5269','5285','5471','6202','6239','6243','6257','6271','6415'
+            ,'6451','6515','6525','6531','6533','6552','6573','6756','8016','8028','8081'
+            ]
+
+print(len(companys))
+
+df = ownership(companys)
+
+print(df)
 
 # if __name__ == "__main__":
 #     stockcode = '5483'
